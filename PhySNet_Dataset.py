8d621b9e37d02218fd535397f986ad1247945198
LiDuanAtGlasgow <6869995@gmail.com>
2020-12-27 12:21:55 +0000

    Update

--- PhySNet_Dataset.py
+++ PhySNet_Dataset.py
@@ -3,10 +3,9 @@ from torch.vision import VisionDataset
 from PIL import Image
 import os
 from typing import Any,Callable,Dict,IO,Optional,Tuple,Union
+import pandas as pd
 
 class PhySNet_Dataset(VisionDataset):
-    training_file=''
-    test_file=''
     classes=[]
     def __init__(self,root:str,train:bool=True,trainsform:Optional[Callable]=None,target_transform:Optional[Callable]=None)->None:
         super(PhySNet_Dataset,self).__init__(root,transform=trainsform,target_transform=target_transform)
@@ -15,11 +14,10 @@ class PhySNet_Dataset(VisionDataset):
             data_file=self.train_file
         else:
             data_file=self.test_file
-        self.data,self.targets=torch.load(os.path.join(self.processed_folder,data_file))
     
     def __getitem__(self,index:int)->Tuple[Any,Any]:
         img,target=self.data[index],int(self.targets[index])
-        img-Image.fromarray(img.numpy(),mode='L')
+        img=Image.fromarray(img.numpy(),mode='L')
         if self.transform is not None:
             img=self.transform(img)
         if self.target_transform is not None:
