6cd0581e5484dca2d3313922bd3adebf4905204b
LiDuanAtGlasgow <6869995@gmail.com>
2021-01-21 17:08:46 +0000

    Update

--- Parameters.py
+++ Parameters.py
@@ -1,6 +1,91 @@
 #type:ignore
-
+import os 
+import json
+import csv
+import pandas as pd
+import numpy as np
 def get_parameters(parameters):
-    zero=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    #one=[-0.6562325365004666, -0.37208199420623933, 0.13608980252767933, -0.40527951675271434, 0.35566735305056474, -0.9806750401256431, -0.0068826079995807365, -0.01723998775968727, -0.2483508005633004, -0.2673377984522245, 0.1790400753512842, 0.5942944289110346, 0.37821280963786963, 0.5870387555320677, -0.9165352557994316, -0.9171428571428571, -0.87224]
-    return [zero,parameters]
+    data=pd.read_csv('./parameters.csv')
+    index=data.shape[0]
+    with open ('./parameters.csv','a') as f:
+        csv_writer=csv.writer(f)
+        csv_writer.writerow((str(index),parameters[0],parameters[1],parameters[2],parameters[3],parameters[4],parameters[5],parameters[6],parameters[7],parameters[8],parameters[9],parameters[10],
+        parameters[11],parameters[12],parameters[13],parameters[14],parameters[15],parameters[16]))
+    print ('get_parameters finished!')
+            
+
+class Get_ArcSim_Script():
+    def __init__(self,bend_stiffness,winds,density,index):
+        self.bend_stiffness=bend_stiffness
+        self.winds=winds
+        self.density=density
+        self.index=index
+        self.material_file='/home/kentuen/ArcSim_Project/arcsim-0.2.1/materials/materials_bayoptim_%d.json'%self.index
+        self.conf_file='/home/kentuen/ArcSim_Project/arcsim-0.2.1/conf/conf_bayoptim_%d.json'%self.index
+    
+    def make_materials(self):
+        data={}
+        data['density']=self.density
+        data['stretching']=[]
+        data['stretching'].append([2002.08618, 0.345588, 2027.48559, 19.189022])
+        data['stretching'].append([2107.22753, 36.371597, 2171.73339, 16.904713])
+        data['stretching'].append([2107.29882, 57.936157, 2153.30712, 24.392872])
+        data['stretching'].append([2107.73291, 28.389059, 2132.72607, 101.462021])
+        data['stretching'].append([2106.93750, 70.149529, 2121.87353, 38.054455])
+        data['stretching'].append([2107.30786, 61.216290, 2150.47680, 37.668823])
+        data['bending']=[]
+        data['bending'].append([2.96381065e-04,8.63785905e-05,3.37376800e-04,4.05180236e-04,1.26734524e-04])
+        data['bending'].append([2.20892369e-04,2.63036220e-05,1.61146563e-04,1.60999798e-04,5.75423709e-05])
+        data['bending'].append([7.86545722e-05,1.17325447e-04,1.94304753e-04,7.93636268e-05,1.45584206e-04])
+        with open (self.material_file,'w') as outputfile:
+            json.dump(data,outputfile)
+    
+    def make_conf(self):
+        data={}
+        data['frame_time']=0.04
+        data["frame_steps"]=8
+        data["duration"]=20
+        data["cloths"]=[]
+        data['cloths'].append({
+        "mesh": "meshes/square.obj",
+        "transform": {
+        "translate": [0, 0, 0],
+        "rotate": [120, 1, 1, 1]},
+        "materials": [{
+        "data": 'materials/materials_bayoptim_'+str(self.index)+'.json',
+        "thicken": 1,
+        "strain_limits": [0.95, 1.05]
+        }],
+        "remeshing": {
+        "refine_angle": 0.3,
+        "refine_compression": 0.01,
+        "refine_velocity": 1,
+        "size": [20e-3, 500e-3],
+        "aspect_min": 0.2
+        }
+        })
+        data["motions"]=[]
+        data["handles"]=[{"nodes": [2,3]}]
+        data["gravity"]=[0, 0, -9.8]
+        data["wind"]={"velocity": [self.winds,self.winds, 0]}
+        data['magic']={"repulsion_thickness": 10e-3, "collision_stiffness": 1e6}
+        with open (self.conf_file,'w') as outputfile:
+            json.dump(data,outputfile)
+    
+    def forward(self):
+        self.make_materials()
+        self.make_conf()
+        print ('make_materials and make_conf are completed')
+
+def read_parameters(csv_file='./parameters.csv'):
+    data=pd.read_csv(csv_file)
+    (width,length)=data.shape
+    print ('data:',data.shape)
+    paramters=np.zeros((width,length-1))
+    for i in range(len(paramters)):
+        for t in range (len(paramters[i])):
+            paramters[i][t]=data.iloc[i,t+1]
+    return paramters
+
+
+
--- main.py
+++ main.py
@@ -35,7 +35,7 @@ from botorch.sampling import SobolQMCNormalSampler
 from botorch.acquisition import UpperConfidenceBound
 from botorch.optim import optimize_acqf
 from botorch.utils import standardize
-from Parameters import get_parameters
+from Parameters import get_parameters,Get_ArcSim_Script,read_parameters
 
 cuda=torch.cuda.is_available()
 
@@ -798,9 +798,9 @@ physnet_classes=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','1
 colors=['#ff7f01','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf','#585957','#232b08','#bec03d','#7a8820','#252f2d',
 '#f4edb5','#6f4136','#e0dd98','#716c29','#14221a','#596918','#9cb45c','#6f2929','#22341f','#706719','#706719','#8f3e34','#c46468','#b4b4be','#252f2d','#7a8820']
 '''
-physnet_classes=['0','1','2']
-colors=['#ff7f01','#2ca02c','#d62728']
-numbers=[0,1,2]
+physnet_classes=['0','1','2','3']
+colors=['#ff7f01','#2ca02c','#d62728','#9467bd']
+numbers=[0,1,2,3]
 print ('physnet_classes:',len(physnet_classes))
 print ('color:',len(colors))
 
@@ -837,8 +837,7 @@ def extract_embeddings(dataloader,model):
 def Bayesian_Search(model,dataloader,parameters):
     d=17
     bounds = torch.stack([-torch.ones(d), torch.ones(d)])
-    phydises=[]
-    bay_numbers=[1,2]
+    bay_numbers=[1,2,3]
 
     with torch.no_grad():
         model.eval()
@@ -869,6 +868,7 @@ def Bayesian_Search(model,dataloader,parameters):
             k+=1
     parameters=torch.Tensor(parameters)
     physical_distance=torch.Tensor(distances).unsqueeze(dim=1)
+    print ('physical_distance:',physical_distance)
 
     gp = SingleTaskGP(parameters, physical_distance)
     mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
@@ -1063,7 +1063,7 @@ if par.train_mode==5:
     batch_size=32
     kwargs={'num_workers':4,'pin_memory':True} if cuda else {}
     model=torch.load(model_path+'model.pth')
-    mean,std=0.10925024,0.24741879
+    mean,std=0.09553191,0.24077791
     file_path='./BayOptim_session/'
     data='img/'
     csv='target/target.csv'
@@ -1075,35 +1075,24 @@ if par.train_mode==5:
     dataloader=DataLoader(dataset,batch_size=batch_size,shuffle=True,**kwargs)
     embeddings,labels=extract_embeddings(dataloader,model)
     plot_embeddings(embeddings,labels)
-    bending_stiffness=[[4.29139383e-04,2.51599026e-04,2.90418223e-04,3.72064753e-04,5.00710154e-05],[4.02705274e-04,5.28828884e-05,1.07060601e-04,1.94618420e-04,8.11112628e-05],[1.30253901e-04,4.83004931e-05,8.37374780e-05,6.56922074e-05,1.60356212e-04]]
-    winds=1.3194
-    density=0.1029
-    normalized_bending_stiffness=normalize_bend(bending_stiffness,-1,1)
-    normalized_winds=normalize(winds,1,6,-1,1)
-    normalized_density=normalize(density,0.1,0.17,-1,1)
-    parameters=[]
-    for i in range (len(normalized_bending_stiffness)):
-        for t in range (len(normalized_bending_stiffness[i])):
-            parameters.append(normalized_bending_stiffness[i][t])
-    parameters.append(normalized_density)
-    parameters.append(normalized_winds)
-    print ('parameterts:',parameters)
-    parameters=get_parameters(parameters)
+    parameters=read_parameters()
     #------------------------------------------------------------#
-    parameters=Bayesian_Search(model,dataloader,parameters)
+    parameter=Bayesian_Search(model,dataloader,parameters)
+    parameter=parameter.cpu().detach().numpy()
     #------------------------------------------------------------#
     denormalized_bending_stiffness=np.zeros((3,5))
     for i in range (len(denormalized_bending_stiffness)):
         for t in range (len(denormalized_bending_stiffness[i])):
-            denormalized_bending_stiffness[i][t]=parameters[0][i*5+t]
+            denormalized_bending_stiffness[i][t]=parameter[0][i*5+t]
     denormalized_bending_stiffness=denormalize_bend(denormalized_bending_stiffness,-1,1)
-    denormalized_density=parameters[0][15]
-    denormalized_winds=parameters[0][16]
+    denormalized_density=parameter[0][15]
+    denormalized_winds=parameter[0][16]
     denormalized_density=denormalize(denormalized_density,0.1,0.17,-1,1)
     denormalized_winds=denormalize(denormalized_winds,1,6,-1,1)
-    print ('denormalized_bending_stiffness:',denormalized_bending_stiffness)
-    print ('denormalized_density:',denormalized_density)
-    print ('denormalized_winds:',denormalized_winds)
+    print ('denormlized_winds:',denormalized_winds)
+    get_arcsim_script=Get_ArcSim_Script(denormalized_bending_stiffness,denormalized_winds,denormalized_density,len(parameters)+1)
+    get_arcsim_script.forward()
+    get_parameters(np.squeeze(parameter))
 
 print ('PhySNet Completed!')
 
